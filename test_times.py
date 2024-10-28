c3e71f8b7c5ef2b346355d7f8288c66e040e265f
hananAlaskar <ucaphfh@ucl.ac.uk>
2024-10-28 10:39:49 +0000

    do some clean up

--- test_times.py
+++ test_times.py
@@ -32,5 +32,10 @@ def test_adjacent_times():
     assert compute_overlap_time(t_1, t_2) == expected_adjacent_times
 
 
-
-
+def test_night():
+    t_1 = time_range("2010-01-12 10:00:00", "2010-01-13 02:00:00")
+    t_2 = time_range("2010-01-13 01:00:00", "2010-01-13 03:45:00")
+    
+    expected_night_times = [("2010-01-13 01:00:00", "2010-01-13 02:00:00")]
+    
+    assert compute_overlap_time(t_1, t_2) == expected_night_times
