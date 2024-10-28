bb6d5253c463439c342ecec9c88bae434d4fcb7e
hananAlaskar <ucaphfh@ucl.ac.uk>
2024-10-28 10:22:26 +0000

    add night_fixed test

--- test_times_fixed.py
+++ test_times_fixed.py
@@ -15,11 +15,8 @@ def test_no_overlap_for_fixed():
 def test_two_intervals_for_fixed():
     t_1 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60)
     t_2 = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    
-    expected_two_intervals = [("2010-01-12 10:30:00", '2010-01-12 10:37:00'),
-                               ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
 
-    assert compute_overlap_time_fixed(t_1, t_2) == expected_two_intervals
+    assert compute_overlap_time_fixed(t_1, t_2) == t_2
 
 
 def test_adjacent_times_for_fixed():
@@ -29,3 +26,12 @@ def test_adjacent_times_for_fixed():
     expected_adjacent_times = []
     
     assert compute_overlap_time_fixed(t_1, t_2) == expected_adjacent_times
+
+
+def test_night_fixed():
+    t_1 = time_range("2010-01-12 10:00:00", "2010-01-13 02:00:00")
+    t_2 = time_range("2010-01-13 01:00:00", "2010-01-13 03:45:00")
+    
+    expected_night_times = [("2010-01-13 01:00:00", "2010-01-13 02:00:00")]
+    
+    assert compute_overlap_time_fixed(t_1, t_2) == expected_night_times
