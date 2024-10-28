ce73d3687c27d0daf1454403459e4dd896163341
hananAlaskar <ucaphfh@ucl.ac.uk>
2024-10-28 11:29:08 +0000

    add some code to test the CI

--- times.py
+++ times.py
@@ -29,3 +29,8 @@ if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(compute_overlap_time(large, short))
+
+    ## TO test the CI 
+    invaild_time = time_range("2010-01-13 10:00:00", "2010-01-12 12:00:00")
+    print(invaild_time)
+ 
